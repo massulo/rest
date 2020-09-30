89c86d1afe9e9b3488b50515e0f3c58753e9ab9c
Massulo <ramon.massulo@yahoo.com.br>
2020-09-29 23:20:34 -0300

    CORS e BodyParser

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
-const rotaAlunos = require('./routes/alunos');
+const rotaProdutos = require('./routes/produtos');
+const rotaPedidos = require('./routes/pedidos');
 
-app.use('/alunos', rotaAlunos);
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false})); //apenas dados simples
+app.use(bodyParser.json()); // Json de entrada no body
+
+app.use((req, res , next) =>{
+    res.header('Acces-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Requrested-With, Content-Type, Accept, Authorization'
+        );
+
+        if (req.method === 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).send({});
+        }
+
+        next();
+}) 
+
+app.use('/produtos', rotaProdutos);
+app.use('/pedidos', rotaPedidos);
+
+//Quando não encontra nenhuma rota
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
 
 module.exports = app;
